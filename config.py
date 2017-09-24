4fc96dba7887f025f9837008531e522fc5f65577
wxor <32234633+wxor@users.noreply.github.com>
2017-09-24 16:01:10 +1000

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ sTime = 600
 
 #API KEYS
 consumer_key = 'K8RbLDgYmCKhaJdJD7yxLY7l'
-consumer_secret = 'Y8TKoKudnLRZLljVcSzaZrXOQxgNM6JOjJyB5WHre7Qrd7WAeM'
+consumer_secret = '8TKoKudnLRZLljVcSzaZrXOQxgNM6JOjJyB5WHre7Qrd7WAeM'
 access_token = '09683756205719552-k6xUZLCZ00OtPVeOzTwa6JPEcTAj0wh'
 access_secret = 'Moy7qywC06VhKnOeMMvgnZmrskXiRWDUXhmisG2Uaq9I'
