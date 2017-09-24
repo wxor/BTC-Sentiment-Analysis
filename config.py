15dc205579b6e475485e8312477583c4294c0d23
wxor <32234633+wxor@users.noreply.github.com>
2017-09-24 15:45:31 +1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#Adjust sample time in seconds
+sTime = 600
+
+#API KEYS
+consumer_key = 'K8RbLDgYmCKhaJdJD7yxLY7l'
+consumer_secret = 'Y8TKoKudnLRZLljVcSzaZrXOQxgNM6JOjJyB5WHre7Qrd7WAeM'
+access_token = '09683756205719552-k6xUZLCZ00OtPVeOzTwa6JPEcTAj0wh'
+access_secret = 'Moy7qywC06VhKnOeMMvgnZmrskXiRWDUXhmisG2Uaq9I'
